b10afa3cf0b1bdc2cb47a00e15c20e5449be75f7
Gaurav <aggarwalgaurav353@gmail.com>
2024-10-07 15:19:31 +0530

    Few changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import "./App.css";
-import FunctionalComponent from "./Components/FunctionalComponent";
-import { ClassComponent, ClassComponent1 } from "./Components/ClassComponent";
-import Click from "./Components/click";
-import Counter from "./Components/Counter";
-import ParentComponent from "./Components/ParentComponent";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World, my name is Gaurav</h1>;
-      <FunctionalComponent></FunctionalComponent>
-      <ClassComponent />
-      <ClassComponent1 />
-      <Click />
-      <Counter />
-      <ParentComponent />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
 
-export default App;
+export default App
